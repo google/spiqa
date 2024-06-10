ee579e65ef12bb8385fc3514118f737f79bb9e0e
Subhashini Venugopalan <vsubhashini@google.com>
2024-06-10 22:26:24 +0000

    Add code for evals and metric computations from model responses.

---
+++ Evals/test-C/open_models/InstructBLIP_QA_test-C_Evaluation_Image+Caption.py
@@ -0,0 +1,134 @@
+from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
+import torch
+import json
+import random
+from PIL import Image
+import os
+import argparse
+
+qasper_filtered_annotations_path = '../Datasets/QASPER/qasper_0520.json'
+with open(qasper_filtered_annotations_path, "r") as f:
+  qasper_data = json.load(f)
+
+_QASPER_IMAGE_ROOT = "../Datasets/QASPER/qasper_figures/all"
+
+def prepare_inputs(paper, question_idx):
+    all_figures = [element['file'] for element in paper['figures_and_tables']]
+    referred_figures = list(set(paper['referred_figures_tables'][question_idx]))
+    all_figures_captions_dict = {}
+    for element in paper['figures_and_tables']:
+        all_figures_captions_dict.update({element['file']: element['caption']})
+    
+    if paper['answer'][question_idx]['free_form_answer'] != '': 
+        answer = paper['answer'][question_idx]['free_form_answer']
+    elif paper['answer'][question_idx]['yes_no'] != None:
+        if paper['answer'][question_idx]['yes_no'] == False:
+            answer = 'No'
+        elif paper['answer'][question_idx]['yes_no'] == True:
+            answer = 'Yes'
+        else:
+            raise ValueError
+    else:
+        raise ValueError
+    
+    referred_figures_captions = []
+    for figure in referred_figures:
+        referred_figures_captions.append(all_figures_captions_dict[figure])
+
+    return answer, all_figures, referred_figures, referred_figures_captions
+
+
+_PROMPT_1 = "Caption: <caption>. Is the input image and caption helpful to answer the following question. Answer in one word - Yes or No. Question: <question>. "
+_PROMPT_2 = "Caption: <caption>. Please provide a brief answer to the following question after looking into the input image and caption. Question: <question>."
+
+def infer_instructblip(qasper_data, args):
+
+  processor = InstructBlipProcessor.from_pretrained(args.model_id)
+  model = InstructBlipForConditionalGeneration.from_pretrained(args.model_id, load_in_4bit=True, torch_dtype=torch.float16)
+
+  _RESPONSE_ROOT = args.response_root
+  os.makedirs(_RESPONSE_ROOT, exist_ok=True)
+
+  for paper_id, paper in sorted(qasper_data.items(), key=lambda x: random.random()):
+    if os.path.exists(os.path.join(_RESPONSE_ROOT, str(paper_id) + '_response.json')):
+      continue
+    response_paper = {}
+
+    try:
+      for question_idx, question in enumerate(paper['question']):
+
+        answer, all_figures, referred_figures, referred_figures_captions = prepare_inputs(paper, question_idx)
+
+        answer_dict = {}
+
+        for _idx, figure in enumerate(referred_figures):
+          
+          caption = referred_figures_captions[_idx]
+              
+          instructblip_prompt_1 = _PROMPT_1.replace('<caption>', caption).replace('<question>', question)
+          instructblip_prompt_2 = _PROMPT_2.replace('<caption>', caption).replace('<question>', question)
+            
+          image = Image.open(os.path.join(_QASPER_IMAGE_ROOT, paper['arxiv_id'], figure))
+          image = image.resize((args.image_resolution, args.image_resolution))
+            
+          inputs_1 = processor(images=image, text=instructblip_prompt_1, return_tensors="pt").to(device="cuda", dtype=torch.float16)
+
+          # autoregressively generate an answer
+          outputs_1 = model.generate(
+                  **inputs_1,
+                  num_beams=1,
+                  max_new_tokens=50,
+                  min_length=1,
+                  top_p=0.9,
+                  repetition_penalty=1.5,
+                  length_penalty=1.0,
+                  temperature=1,
+          )
+          outputs_1[outputs_1 == 0] = 2 # this line can be removed once https://github.com/huggingface/transformers/pull/24492 is fixed
+          generated_text_1 = processor.batch_decode(outputs_1, skip_special_tokens=True)[0].strip()
+
+          inputs_2 = processor(images=image, text=instructblip_prompt_2, return_tensors="pt").to(device="cuda", dtype=torch.float16)
+
+          # autoregressively generate an answer
+          outputs_2 = model.generate(
+                  **inputs_2,
+                  num_beams=5,
+                  max_new_tokens=256,
+                  min_length=1,
+                  top_p=0.9,
+                  repetition_penalty=1.5,
+                  length_penalty=1.0,
+                  temperature=1,
+          )
+          outputs_2[outputs_2 == 0] = 2 # this line can be removed once https://github.com/huggingface/transformers/pull/24492 is fixed
+          generated_text_2 = processor.batch_decode(outputs_2, skip_special_tokens=True)[0].strip()
+          
+
+          answer_dict.update({figure: [generated_text_1, generated_text_2]})
+      
+          print(answer_dict[figure])
+          print('-----------------')
+
+        question_key = paper['question_key'][question_idx]
+        response_paper.update({question_key: {'question': question, 'response': answer_dict,
+                                              'referred_figures_names': referred_figures, 'answer': answer}})   
+
+    except:
+        print('Error in generating.')
+        processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
+        model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b", load_in_4bit=True, torch_dtype=torch.float16)
+        continue
+
+    with open(os.path.join(_RESPONSE_ROOT, str(paper_id) + '_response.json'), 'w') as f:
+      json.dump(response_paper, f)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Evaluate on Qasa/Qasper.')
+    parser.add_argument('--model_id', type=str, default="Salesforce/instructblip-vicuna-7b", help='InstructBLIP Model id.')
+    parser.add_argument('--response_root', type=str, help='Response Root path.')
+    parser.add_argument('--image_resolution', type=int, help='Response Root path.')
+    args = parser.parse_args()
+    
+    
+    infer_instructblip(qasper_data, args)
---
+++ Metrics/pycocoevalcap_spiqa/eval.py
@@ -0,0 +1,81 @@
+__author__ = 'tylin'
+from .tokenizer.ptbtokenizer import PTBTokenizer
+from .bleu.bleu import Bleu
+from .meteor.meteor import Meteor
+from .rouge.rouge import Rouge
+from .cider.cider import Cider
+from .spice.spice import Spice
+
+
+class COCOEvalCap:
+    def __init__(self, coco, cocoRes):
+        self.evalImgs = []
+        self.eval = {}
+        self.imgToEval = {}
+        self.coco = coco
+        self.cocoRes = cocoRes
+        self.params = {'image_id': coco.getImgIds()}
+
+    def evaluate(self, eval_metrics=['Bleu', 'METEOR', 'ROUGE_L', 'CIDEr', 'SPICE']):
+        imgIds = self.params['image_id']
+        # imgIds = self.coco.getImgIds()
+        gts = {}
+        res = {}
+        for imgId in imgIds:
+            gts[imgId] = self.coco.imgToAnns[imgId]
+            res[imgId] = self.cocoRes.imgToAnns[imgId]
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('tokenization...')
+        tokenizer = PTBTokenizer()
+        gts  = tokenizer.tokenize(gts)
+        res = tokenizer.tokenize(res)
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('setting up scorers...')
+        scorers = []
+        for eval_metric in eval_metrics:
+            if eval_metric == 'Bleu':
+                scorers.append((Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]))
+            elif eval_metric == 'METEOR':
+                scorers.append((Meteor(),"METEOR"))
+            elif eval_metric == 'ROUGE_L':
+                scorers.append((Rouge(),"ROUGE_L"))
+            elif eval_metric == 'CIDEr':
+                scorers.append((Cider(),"CIDEr"))
+            elif eval_metric == 'SPICE':
+                scorers.append((Spice(),"SPICE"))
+
+        # =================================================
+        # Compute scores
+        # =================================================
+        for scorer, method in scorers:
+            print('computing %s score...'%(scorer.method()))
+            score, scores = scorer.compute_score(gts, res)
+            if type(method) == list:
+                for sc, scs, m in zip(score, scores, method):
+                    self.setEval(sc, m)
+                    self.setImgToEvalImgs(scs, gts.keys(), m)
+                    print("%s: %0.3f"%(m, sc))
+            else:
+                self.setEval(score, method)
+                self.setImgToEvalImgs(scores, gts.keys(), method)
+                print("%s: %0.3f"%(method, score))
+        self.setEvalImgs()
+
+    def setEval(self, score, method):
+        self.eval[method] = score
+
+    def setImgToEvalImgs(self, scores, imgIds, method):
+        for imgId, score in zip(imgIds, scores):
+            if not imgId in self.imgToEval:
+                self.imgToEval[imgId] = {}
+                self.imgToEval[imgId]["image_id"] = imgId
+            self.imgToEval[imgId][method] = score
+
+    def setEvalImgs(self):
+        self.evalImgs = [eval for imgId, eval in self.imgToEval.items()]
